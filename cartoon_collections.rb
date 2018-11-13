4b0cd481ec33ee0cd64e43877eab7d7948a0b71e
Kah Yap <kahyap13@gmail.com>
2018-11-13 22:22:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index { |ele, idx| puts "#{idx + 1}. #{ele}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(summon)# code an argument here
   # Your code here
+  summon.map { |ele| "#{ele.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
